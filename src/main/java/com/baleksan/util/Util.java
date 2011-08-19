63bb7c41864bbf19bea6f15419d3fba93bb2cf0d
Boris Aleksandrovsky <baleksan@gmail.com>
2011-08-19 10:36:30 -0700

    added string encoding utils

--- src/main/java/com/baleksan/util/Util.java
+++ src/main/java/com/baleksan/util/Util.java
@@ -3,15 +3,30 @@ package com.baleksan.util;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+
 
 /**
  * @author <a href="mailto:baleksan@yammer-inc.com" boris/>
  */
-public class Util {
+public class Util
+{
     public static final String EOL = System.getProperty("line.separator");
     public static final Charset UTF8 = Charset.forName("UTF-8");
 
-    public static String list2Str(List stringList) {
+    public static final Map<Integer, String> encodingSeparators;
+
+    static {
+        encodingSeparators = new HashMap<Integer, String>();
+        encodingSeparators.put(0, ";");
+        encodingSeparators.put(1, ":");
+        encodingSeparators.put(2, "/");
+    }
+
+
+    public static String list2Str(List stringList)
+    {
         StringBuilder builder = new StringBuilder();
         for (Object str : stringList) {
             builder.append(str.toString());
@@ -21,7 +36,8 @@ public class Util {
         return builder.toString().trim();
     }
 
-    public static String set2Str(Set stringList) {
+    public static String set2Str(Set stringList)
+    {
         StringBuilder builder = new StringBuilder();
         for (Object str : stringList) {
             builder.append(str.toString());
@@ -31,7 +47,8 @@ public class Util {
         return builder.toString().trim();
     }
 
-    public static String array2Str(Object[] objectArray) {
+    public static String array2Str(Object[] objectArray)
+    {
         StringBuilder builder = new StringBuilder();
         for (Object obj : objectArray) {
             builder.append(obj.toString());
@@ -48,7 +65,8 @@ public class Util {
      * @param ex
      * @return string which represents the stack trace
      */
-    public static String formatStackTrace(Exception ex) {
+    public static String formatStackTrace(Exception ex)
+    {
         StringBuilder builder = new StringBuilder();
         builder.append(ex.getMessage() == null ? ex.getClass().getName() : ex.getMessage());
         builder.append(EOL);
@@ -62,6 +80,44 @@ public class Util {
             }
         }
         return builder.toString();
+    }
+
+
+    /**
+     * Normalizes name for use in string encoding
+     *
+     * @param str
+     * @param list of separators to normalize out
+     * @return
+     */
+    public static String normalizeName(String str, String... separators)
+    {
+        for (String separator : separators) {
+            str = str.replaceAll(separator, "_");
+        }
+
+        return str;
+    }
+
+    /**
+     * Normalizes name for use in string encoding
+     *
+     * @param str
+     * @param list of separators to normalize out
+     * @return
+     */
+    public static String normalizeName(String str)
+    {
+        for (String separator : encodingSeparators.values()) {
+            str = str.replaceAll(separator, "_");
+        }
+
+        return str;
+    }
+
+    public static String getSeparator(int level)
+    {
+        return encodingSeparators.get(level);
     }
 
 }
